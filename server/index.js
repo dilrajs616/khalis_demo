d2fc0d6b087d3e34f40d6cbb96200f3a1a40c7cb
dilrajs616 <dilraj082@gmail.com>
2024-09-06 21:44:39 +0530

    localhost port 3000 done

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-// this is the entry point
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
