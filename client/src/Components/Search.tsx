afbb5251e753d65af0e8243f24269c990abb4ea5
simarjot <simarjot0032@gmail.com>
2024-09-10 01:37:41 +0530

    qdded time changing featue and wirking more on that

--- client/src/Components/Search.tsx
+++ client/src/Components/Search.tsx
@@ -1,10 +1,44 @@
 import Navbar from "./Navbar";
 import "../SCSS/Search.scss";
+import { useState } from "react";
+import toast from "react-hot-toast";
 export default function Search() {
+  const [timingForMic, settimingForMic] = useState(5);
+  function handleTimingChange(time: number) {
+    settimingForMic(time);
+    toast.success(`Mic Time Changed to ${time} seconds`);
+  }
   return (
     <>
       <div className="search-parent-container">
         <Navbar />
+        <div className="content-container">
+          <div className="timing-tab-container">
+            <label htmlFor="" className="timing-label">
+              Select Timing For Mic:
+            </label>
+            <div
+              className={
+                timingForMic === 5
+                  ? "timing-tab timing-tab-active"
+                  : "timing-tab"
+              }
+              onClick={() => handleTimingChange(5)}
+            >
+              5 sec
+            </div>
+            <div
+              className={
+                timingForMic === 10
+                  ? "timing-tab timing-tab-active"
+                  : "timing-tab"
+              }
+              onClick={() => handleTimingChange(10)}
+            >
+              10 sec
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
