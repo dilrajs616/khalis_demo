d6310cbcf78c021ad68d7077eee332a4848c03e9
simarjot <simarjot0032@gmail.com>
2024-09-10 18:23:21 +0530

    some updated and added some more stuff like mic bnt and started on coding for recording

--- client/src/Components/Search.tsx
+++ client/src/Components/Search.tsx
@@ -2,18 +2,19 @@ import Navbar from "./Navbar";
 import "../SCSS/Search.scss";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { CiMicrophoneOn } from "react-icons/ci";
 export default function Search() {
-  const [timingForMic, settimingForMic] = useState(5);
-  function handleTimingChange(time: number) {
-    settimingForMic(time);
-    toast.success(`Mic Time Changed to ${time} seconds`);
-  }
+  // const [timingForMic, settimingForMic] = useState(5);
+  // function handleTimingChange(time: number) {
+  //   settimingForMic(time);
+  //   toast.success(`Mic Time Changed to ${time} seconds`);
+  // }
   return (
     <>
       <div className="search-parent-container">
         <Navbar />
         <div className="content-container">
-          <div className="timing-tab-container">
+          {/* <div className="timing-tab-container">
             <label htmlFor="" className="timing-label">
               Select Timing For Mic:
             </label>
@@ -37,6 +38,17 @@ export default function Search() {
             >
               10 sec
             </div>
+          </div> */}
+          <div className="main-heading">Search Gurbani</div>
+          <div className="search-input-box-container">
+            <input
+              type="search"
+              placeholder="Enter your input please"
+              id="search-box"
+            />
+            <button className="mic-btn">
+              <CiMicrophoneOn size={45} />
+            </button>
           </div>
         </div>
       </div>
