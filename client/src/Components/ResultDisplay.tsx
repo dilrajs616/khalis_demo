35e1cd81b9c38419f89c4c446a78f501c13a44ab
simarjot <simarjot0032@gmail.com>
2024-09-19 23:04:44 +0530

    Reapply "added mic visual applieng and mic working detection"
    This reverts commit 3c18efbd3547689b9f1e43368367be88fbad3324.

--- client/src/Components/ResultDisplay.tsx
+++ client/src/Components/ResultDisplay.tsx
@@ -2,26 +2,35 @@ import { useEffect, useState } from "react";
 import "../SCSS/ResultDisplay.scss";
 interface Props {
   resultsFromBaniDB: any;
+  searchType: string;
 }
-export default function ResultDisplay({ resultsFromBaniDB }: Props) {
-  const [verse, setverse] = useState(null);
-
+export default function ResultDisplay({
+  resultsFromBaniDB,
+  searchType,
+}: Props) {
+  console.log(resultsFromBaniDB);
   return (
     <>
       <div className="result-display-container">
         <div className="result-display-container-heading">Results</div>
-        {resultsFromBaniDB.map((gurbani: any, index: number) => {
-          return (
-            <div className="result-display-container" key={index}>
-              <div className="result-display-gurbani-name">
-                {gurbani.verse.unicode}
-              </div>
-              <div className="result-display-gurbani-ang">
-                Ang Number -{gurbani.pageNo}
-              </div>
-            </div>
-          );
-        })}
+        {searchType === "gurbani"
+          ? resultsFromBaniDB.verses.map((gurbani: any, index: number) => {
+              return (
+                <div className="result-display-container" key={index}>
+                  <div className="result-display-gurbani-name">
+                    {gurbani.verse.unicode}
+                  </div>
+                  <div className="result-display-gurbani-ang">
+                    Ang Number -{gurbani.pageNo}
+                  </div>
+                </div>
+              );
+            })
+          : resultsFromBaniDB.verses.map((bani: any) => {
+              return (
+                <div className="gurbani-line">{bani.verse.verse.unicode}</div>
+              );
+            })}
       </div>
     </>
   );
