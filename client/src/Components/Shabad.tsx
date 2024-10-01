380a4160f2c474a8335f6d206f7481ec1a2e270b
simarjot <simarjot0032@gmail.com>
2024-10-01 10:48:13 +0530

    corrected the error for fetching the data for shabad

--- client/src/Components/Shabad.tsx
+++ client/src/Components/Shabad.tsx
@@ -7,33 +7,37 @@ export default function Shabad() {
   const { shabad } = useContext(ShabadContext);
   const [shabadData, setShabadData] = useState<any>();
 
-  useEffect(() => {
-    async function getShabad() {
-      try {
-        const response = await fetch(
-          `https://api.banidb.com/v2/shabads/${shabad}`
-        );
-        if (!response.ok) {
-          console.log(response.status);
-        } else {
-          const data = await response.json();
-          console.log(data.verses[0].verse);
-          setShabadData(data);
-        }
-      } catch (e: any) {
-        console.log(e.message);
+  async function getShabad() {
+    try {
+      const response = await fetch(
+        `https://api.banidb.com/v2/shabads/${shabad}`
+      );
+      if (!response.ok) {
+        console.log(response.status);
+      } else {
+        const data = await response.json();
+        console.log(data.verses[0].verse);
+        setShabadData(data);
       }
+    } catch (e: any) {
+      console.log(e.message);
     }
-    getShabad();
-  }, [0]);
+  }
+  getShabad();
   return (
     <>
       <div className="shabad-container">
         <Navbar />
         <div className="shabad-content-container">
-          {shabadData.verses.map((shabad: any) => {
-            return <p className="shabad-line">{shabad.verse.unicode}</p>;
-          })}
+          {shabadData != null
+            ? shabadData.verses.map((shabad: any, index: number) => {
+                return (
+                  <p className="shabad-line" key={index}>
+                    {shabad.verse.unicode}
+                  </p>
+                );
+              })
+            : undefined}
         </div>
       </div>
     </>
