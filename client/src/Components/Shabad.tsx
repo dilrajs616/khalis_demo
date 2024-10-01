d29ee1af4b683c9c5638d7a23fc1fad020673128
simarjot <simarjot0032@gmail.com>
2024-10-01 10:54:00 +0530

    added some more optimization for less rerendring

--- client/src/Components/Shabad.tsx
+++ client/src/Components/Shabad.tsx
@@ -2,28 +2,30 @@ import Navbar from "./Navbar";
 import "../SCSS/Shabad.scss";
 import { useContext, useEffect, useState } from "react";
 import { ShabadContext } from "../Context/ShabadContext";
+import toast from "react-hot-toast";
 
 export default function Shabad() {
   const { shabad } = useContext(ShabadContext);
   const [shabadData, setShabadData] = useState<any>();
+  useEffect(() => {
+    async function getShabad() {
+      try {
+        const response = await fetch(
+          `https://api.banidb.com/v2/shabads/${shabad}`
+        );
+        if (!response.ok) {
+          console.log(response.status);
+        } else {
+          const data = await response.json();
 
-  async function getShabad() {
-    try {
-      const response = await fetch(
-        `https://api.banidb.com/v2/shabads/${shabad}`
-      );
-      if (!response.ok) {
-        console.log(response.status);
-      } else {
-        const data = await response.json();
-        console.log(data.verses[0].verse);
-        setShabadData(data);
+          setShabadData(data);
+        }
+      } catch (e: any) {
+        console.log(e.message);
       }
-    } catch (e: any) {
-      console.log(e.message);
     }
-  }
-  getShabad();
+    getShabad();
+  }, [0]);
   return (
     <>
       <div className="shabad-container">
